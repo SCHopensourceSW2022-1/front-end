b404e1ca553c586cc7083d62e8e6096095821723
sik9252 <sik9252@naver.com>
2022-05-17 01:17:27 +0900

    chore: reset.css 및 페이지 라우팅
    - reset.css 추가
    - 메인, 학술, 중앙 동아리 페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Reset } from 'styled-reset';
 
-import MainPage from "./Pages/Main/Main";
-import AcademicClubPage from "./Pages/AcademicClub/AcademicClub";
-import CenterClubPage from "./Pages/CenterClub/CenterClub";
+/* Component import */
+import MainLayout from './Components/MainLayout/MainLayout';
+import MainPage from './Pages/Main/Main';
+import AcademicClubPage from './Pages/AcademicClub/AcademicClub';
+import CenterClubPage from './Pages/CenterClub/CenterClub';
+import PageNotFound from './Pages/PageNotFound/PageNotFound';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Reset />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/academicclub" element={<AcademicClubPage />} />
-          <Route path="/centerclub" element={<CenterClubPage />} />
+          <Route
+            path="/"
+            element={<MainLayout View={MainPage} Menu={'AllClub'} />}
+          />
+          <Route
+            path="/academicclub"
+            element={
+              <MainLayout View={AcademicClubPage} Menu={'AcademicClub'} />
+            }
+          />
+          <Route
+            path="/centerclub"
+            element={<MainLayout View={CenterClubPage} Menu={'CenterClub'} />}
+          />
+          <Route path="/*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
