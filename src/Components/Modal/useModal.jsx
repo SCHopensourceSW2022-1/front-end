d7e08034c758f789fb36a66b4ad0edfb4adfe3c0
sik9252 <sik9252@naver.com>
2022-06-10 17:19:18 +0900

    전역 Modal custom hook 추가

---
+++ src/Components/Modal/useModal.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useCallback } from 'react';
+
+const MODAL_OPTION = {
+  isModalShow: false,
+  onOpen: () => {},
+  onClose: () => {},
+  // innerElement: null,
+};
+
+function useModals() {
+  const [modalOption, setModalOption] = useState(MODAL_OPTION);
+
+  const showModal = useCallback(
+    (isModalShow, onOpenCallback, onCloseCallback, innerElement) => {
+      setModalOption((prev) => ({
+        ...prev,
+        isModalShow,
+        onOpen: () => {
+          if (onOpenCallback) onOpenCallback();
+          setModalOption((prev) => ({ ...prev, isModalShow: !isModalShow }));
+        },
+        onClose: () => {
+          if (onCloseCallback) onCloseCallback();
+          setModalOption((prev) => ({ ...prev, isModalShow: !isModalShow }));
+        },
+        // innerElement,
+      }));
+    },
+    [modalOption],
+  );
+
+  return [modalOption, showModal];
+}
+
+export default useModals;
