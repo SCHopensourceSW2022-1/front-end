c57118e653462ab58154394b46b521eda06096b5
sik9252 <sik9252@naver.com>
2022-06-16 16:03:25 +0900

    test: 서버에서 데이터 받아오는 테스트 수행

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -6,55 +6,43 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
 import useModal from '../../Components/Modal/useModal';
 import ModalForm from '../../Components/Modal/ModalForm';
 
 /* import css */
 import '../../Styles/ViewStyles.css';
 
+axios.defaults.withCredentials = true;
+
+const getAllClubList = async () => {
+  const { data } = await axios.get('/data');
+  return data;
+};
+
 const RenderAllContents = () => {
   const [modalOption, showModal] = useModal();
-  const [allClubData, setAllClubData] = useState([
-    {
-      id: 1,
-      tags: ['모집중', '중동', '학과', '보드게임'],
-      img: '/Assets/test.jpg',
-      title: '전체 동아리1',
-      description: '동아리 설명1',
-      intro: '111',
-      activity: '111',
-      method: '111',
-      condition: '111',
-      description_detail: '111',
-      description_add: '111',
+  const [allClubData, setAllClubData] = useState([]);
+
+  const { status, data, error } = useQuery('allClubList', getAllClubList, {
+    retry: 1,
+    refetchOnWindowFocus: false,
+    onSuccess: (data) => {
+      setAllClubData(data);
     },
-    {
-      id: 2,
-      tags: ['모집중', '중동', '예술'],
-      img: '/Assets/test.jpg',
-      title: 'Club Name2',
-      description: '동아리 설명2',
-      intro: '222',
-      activity: '222',
-      method: '222',
-      condition: '222',
-      description_detail: '222',
-      description_add: '222',
+    onError: (error) => {
+      alert(error);
     },
-    {
-      id: 3,
-      tags: ['모집마감', '중동', '여행', '취미'],
-      img: '/Assets/test.jpg',
-      title: 'Club Name3',
-      description: '동아리 설명3',
-      intro: '333',
-      activity: '333',
-      method: '/',
-      condition: '333',
-      description_detail: '333',
-      description_add: '333',
-    },
-  ]);
+  });
+
+  console.log(data);
+
+  // Name: title
+  // ContentsMain: description
+  // ContentsDetali: intro, activity, method, condition
+  // Image: img
 
   const openModal = () => {
     showModal(true, null, null);
@@ -62,28 +50,28 @@ const RenderAllContents = () => {
 
   return (
     <Grid container spacing={2} className="contents-grid">
+      <ModalForm ContentData={allClubData} modalOption={modalOption} />
       {allClubData.map((item) => {
         return (
-          <Grid item lg={3} key={item.id}>
+          <Grid item lg={3} key={item.Id}>
             <Card onClick={openModal}>
               <CardActionArea>
                 <CardMedia
                   component="img"
                   height="140"
-                  image={item.img}
+                  image={item.Image}
                   alt="test.jpg"
                 />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
-                    {item.title}
+                    {item.Name}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    {item.description}
+                    {item.ContentsMain}
                   </Typography>
                 </CardContent>
               </CardActionArea>
             </Card>
-            <ModalForm ContentData={allClubData} modalOption={modalOption} />
           </Grid>
         );
       })}
