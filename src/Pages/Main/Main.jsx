27170cc4fda15ee5dfbe06e32f75e8cd87e50efd
sik9252 <sik9252@naver.com>
2022-06-16 19:09:39 +0900

    feat: react-query를 통해 서버에서 전체 동아리 데이터 받아온 후 렌더링

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Grid from '@mui/material/Grid';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import { useQuery } from 'react-query';
 import axios from 'axios';
@@ -15,8 +16,6 @@ import ModalForm from '../../Components/Modal/ModalForm';
 /* import css */
 import '../../Styles/ViewStyles.css';
 
-axios.defaults.withCredentials = true;
-
 const getAllClubList = async () => {
   const { data } = await axios.get('/data');
   return data;
@@ -25,8 +24,9 @@ const getAllClubList = async () => {
 const RenderAllContents = () => {
   const [modalOption, showModal] = useModal();
   const [allClubData, setAllClubData] = useState([]);
+  const [contentID, setContentID] = useState(1);
 
-  const { status, data, error } = useQuery('allClubList', getAllClubList, {
+  const { status } = useQuery('allClubList', getAllClubList, {
     retry: 1,
     refetchOnWindowFocus: false,
     onSuccess: (data) => {
@@ -37,45 +37,51 @@ const RenderAllContents = () => {
     },
   });
 
-  console.log(data);
-
-  // Name: title
-  // ContentsMain: description
-  // ContentsDetali: intro, activity, method, condition
-  // Image: img
-
-  const openModal = () => {
+  const openModal = (content_id) => {
     showModal(true, null, null);
+    setContentID(content_id);
   };
 
   return (
-    <Grid container spacing={2} className="contents-grid">
-      <ModalForm ContentData={allClubData} modalOption={modalOption} />
-      {allClubData.map((item) => {
-        return (
-          <Grid item lg={3} key={item.Id}>
-            <Card onClick={openModal}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={item.Image}
-                  alt="test.jpg"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {item.Name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {item.ContentsMain}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        );
-      })}
-    </Grid>
+    <>
+      {status === 'loading' ? (
+        <div className="progress">
+          <CircularProgress />
+        </div>
+      ) : (
+        <Grid container spacing={2} className="contents-grid">
+          <ModalForm
+            ContentData={allClubData}
+            modalOption={modalOption}
+            contentID={contentID}
+          />
+          {allClubData.map((item) => {
+            return (
+              <Grid item lg={3} md={4} xs={6} key={item.Id}>
+                <Card onClick={() => openModal(item.Id)}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={`data:image/png;base64,${item.img}`}
+                      alt="test.jpg"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {item.Name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {item.ContentsMain}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
+    </>
   );
 };
 
