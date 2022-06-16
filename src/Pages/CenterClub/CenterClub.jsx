fef4390d7aa04d8dd1217241f087fc41d0101402
sik9252 <sik9252@naver.com>
2022-06-16 19:09:21 +0900

    feat: react-query를 이용해 서버에서 중앙 동아리 데이터 받아온 후 렌더링

--- src/Pages/CenterClub/CenterClub.jsx
+++ src/Pages/CenterClub/CenterClub.jsx
@@ -5,6 +5,10 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Grid from '@mui/material/Grid';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
 import useModal from '../../Components/Modal/useModal';
 import ModalForm from '../../Components/Modal/ModalForm';
@@ -12,117 +16,78 @@ import ModalForm from '../../Components/Modal/ModalForm';
 /* import css */
 import '../../Styles/ViewStyles.css';
 
+const getCenterClubList = async () => {
+  const { data } = await axios.get('/data');
+  return data;
+};
+
 const RenderCenterContents = () => {
   const [modalOption, showModal] = useModal();
-  const [centerClubData, setCenterClubData] = useState([
-    {
-      id: 1,
-      tags: ['모집중', '중동', '학과', '보드게임'],
-      img: '/Assets/test.jpg',
-      title: '중앙 동아리1',
-      description: '동아리 설명1',
-      intro: '보드게임을 좋아하고 즐길 사람 모여라~!',
-      activity: '팀을 맺어 팀 대항전을 진행합니다',
-      method: '/',
-      condition: '보드게임에 관심이 많은 순천향대학교 재학생',
-      description_detail: '동아리 설명1',
-      description_add:
-        '#1, 3월 에타에 올라왔던 보드게임 동아리 모집 공고 기억 하시나요? 그 이후에도 정말 많은 분들이 "추가모집 안하나요?" 라는 문의를 정말 많이 주셨습니다!! 그래서 동아리 내에 더 활기차고 즐거운 분위기와 다양한 사람과 즐기며 게임하자는 동아리의 취지에 맞게 추가모집을 하기로 결정했습니다! 선착순이 아니므로 편안하게 신청해주셔도 됩니다!! 신청하신 폼을 참고하여 동아리 내부사항과 맞는 분을 신규 멤버로 선발할 예정입니다!',
-    },
-    {
-      id: 2,
-      tags: ['모집중', '중동', '예술'],
-      img: '/Assets/test.jpg',
-      title: 'Club Name2',
-      description: '동아리 설명2',
-      intro: '엔터테인먼트/방송/콘텐츠/미디어 분야',
-      activity:
-        '엔터테인먼트/방송/콘텐츠/미디어 분야의 다양한 세부 과정들을 실제로 진행!',
-      method: '/',
-      condition: '엔터테인먼트/방송/콘텐츠/미디어 분야에 관심있는 재학생',
-      description_detail: '동아리 설명2',
-      description_add:
-        '#2, 3월 에타에 올라왔던 보드게임 동아리 모집 공고 기억 하시나요? 그 이후에도 정말 많은 분들이 "추가모집 안하나요?" 라는 문의를 정말 많이 주셨습니다!! 그래서 동아리 내에 더 활기차고 즐거운 분위기와 다양한 사람과 즐기며 게임하자는 동아리의 취지에 맞게 추가모집을 하기로 결정했습니다! 선착순이 아니므로 편안하게 신청해주셔도 됩니다!! 신청하신 폼을 참고하여 동아리 내부사항과 맞는 분을 신규 멤버로 선발할 예정입니다!',
-    },
-    {
-      id: 3,
-      tags: ['모집마감', '중동', '여행', '취미'],
-      img: '/Assets/test.jpg',
-      title: 'Club Name3',
-      description: '동아리 설명3',
-      intro: '저희와 함께 여행 떠나실 나드리분들을 모집합니다.',
-      activity: '여행, 캠핑~~',
-      method: '/',
-      condition:
-        '일상에 지치거나 다양한 분야의 사람들과 교류하고 싶은 순천향대학교 재학생',
-      description_detail: '동아리 설명3',
-      description_add:
-        '#3, 3월 에타에 올라왔던 보드게임 동아리 모집 공고 기억 하시나요? 그 이후에도 정말 많은 분들이 "추가모집 안하나요?" 라는 문의를 정말 많이 주셨습니다!! 그래서 동아리 내에 더 활기차고 즐거운 분위기와 다양한 사람과 즐기며 게임하자는 동아리의 취지에 맞게 추가모집을 하기로 결정했습니다! 선착순이 아니므로 편안하게 신청해주셔도 됩니다!! 신청하신 폼을 참고하여 동아리 내부사항과 맞는 분을 신규 멤버로 선발할 예정입니다!',
-    },
-    {
-      id: 4,
-      tags: ['모집중', '중동', '예술'],
-      img: '/Assets/test.jpg',
-      title: 'Club Name4',
-      description: '동아리 설명4',
-      intro: '엔터테인먼트/방송/콘텐츠/미디어 분야',
-      activity:
-        '엔터테인먼트/방송/콘텐츠/미디어 분야의 다양한 세부 과정들을 실제로 진행!',
-      method: '/',
-      condition: '엔터테인먼트/방송/콘텐츠/미디어 분야에 관심있는 재학생',
-      description_detail: '동아리 설명4',
-      description_add:
-        '#2, 3월 에타에 올라왔던 보드게임 동아리 모집 공고 기억 하시나요? 그 이후에도 정말 많은 분들이 "추가모집 안하나요?" 라는 문의를 정말 많이 주셨습니다!! 그래서 동아리 내에 더 활기차고 즐거운 분위기와 다양한 사람과 즐기며 게임하자는 동아리의 취지에 맞게 추가모집을 하기로 결정했습니다! 선착순이 아니므로 편안하게 신청해주셔도 됩니다!! 신청하신 폼을 참고하여 동아리 내부사항과 맞는 분을 신규 멤버로 선발할 예정입니다!',
-    },
-    {
-      id: 5,
-      tags: ['모집마감', '중동', '여행', '취미'],
-      img: '/Assets/test.jpg',
-      title: 'Club Name5',
-      description: '동아리 설명5',
-      intro: '저희와 함께 여행 떠나실 나드리분들을 모집합니다.',
-      activity: '여행, 캠핑~~',
-      method: '/',
-      condition:
-        '일상에 지치거나 다양한 분야의 사람들과 교류하고 싶은 순천향대학교 재학생',
-      description_detail: '동아리 설명5',
-      description_add:
-        '#3, 3월 에타에 올라왔던 보드게임 동아리 모집 공고 기억 하시나요? 그 이후에도 정말 많은 분들이 "추가모집 안하나요?" 라는 문의를 정말 많이 주셨습니다!! 그래서 동아리 내에 더 활기차고 즐거운 분위기와 다양한 사람과 즐기며 게임하자는 동아리의 취지에 맞게 추가모집을 하기로 결정했습니다! 선착순이 아니므로 편안하게 신청해주셔도 됩니다!! 신청하신 폼을 참고하여 동아리 내부사항과 맞는 분을 신규 멤버로 선발할 예정입니다!',
-    },
-  ]);
+  const [centerClubData, setCenterClubData] = useState([]);
+  const [contentID, setContentID] = useState(1);
 
-  const openModal = () => {
+  const { status } = useQuery('centerClubList', getCenterClubList, {
+    retry: 1,
+    refetchOnWindowFocus: false,
+    onSuccess: (data) => {
+      const fetchedData = [];
+      data.forEach((item) => {
+        if (item.Category === 'center') {
+          fetchedData.push(item);
+          setCenterClubData(fetchedData);
+        }
+      });
+    },
+    onError: (error) => {
+      alert(error);
+    },
+  });
+
+  const openModal = (content_id) => {
     showModal(true, null, null);
+    setContentID(content_id);
   };
 
   return (
-    <Grid container spacing={2} className="contents-grid">
-      <ModalForm ContentData={centerClubData} modalOption={modalOption} />
-      {centerClubData.map((item) => {
-        return (
-          <Grid item lg={3} key={item.id}>
-            <Card onClick={openModal}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={item.img}
-                  alt="test.jpg"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {item.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {item.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        );
-      })}
-    </Grid>
+    <>
+      {status === 'loading' ? (
+        <div className="progress">
+          <CircularProgress />
+        </div>
+      ) : (
+        <Grid container spacing={2} className="contents-grid">
+          <ModalForm
+            ContentData={centerClubData}
+            modalOption={modalOption}
+            contentID={contentID}
+          />
+          {centerClubData.map((item) => {
+            return (
+              <Grid item lg={3} md={4} xs={6} key={item.Id}>
+                <Card onClick={() => openModal(item.Id)}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={`data:image/png;base64,${item.img}`}
+                      alt="test.jpg"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {item.Name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {item.ContentsMain}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
+    </>
   );
 };
 
